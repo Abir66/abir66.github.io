3251ede1810694b5183f5f614c885bb2912221c9
Abir Muhtasim <1905066@ugrad.cse.buet.ac.bd>
2024-08-16 12:25:30 +0600

    Added deploy.yml for Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://abir66.github.io',
   integrations: [tailwind(), mdx(), icon(), svelte()]
 });
